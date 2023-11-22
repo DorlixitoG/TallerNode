005b13672561943d96324be62dda5f4f33ec88f0
Johan1020 <londonojohan02@gmail.com>
2023-11-21 23:22:20 -0500

    pipi

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,13 +1,18 @@
 const Usuario = require("../models/Usuario");
 
 // Ruta para obtener la lista de usuarios
-exports.getUsuarios = async (req, res) => {
+module.exports.getUsuarios = async (req, res) => {
   try {
-    const usuarios = await Usuario.find();
+    const usuarios = await Usuario.find({});
     res.render("usuarios", { usuarios });
   } catch (error) {
     res.status(500).send("Error al obtener la lista de usuarios");
   }
+};
+
+module.exports.mostrar = async function mostrar(req, res) {
+  const usuarios = await usuarios.find({});
+  return res.render("index", { usuarios: usuarios });
 };
 
 // Ruta para agregar un nuevo usuario
